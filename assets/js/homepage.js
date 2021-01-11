49c6c464a313a9852242db12e29c2d9829761fd2
dvicjon <devinvjones@gmail.com>
2021-01-11 16:57:48 -0500

    how to format to JSON

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,7 +1,12 @@
 //this function "fetches" the info (HTTP request) from GitHub API
 //GitHub replies with JSON data
 var getUserRepos = function() {
+    //asynchronous behaviour - outside then inside - fetch request set aside until rest of the code is implemented 
     fetch("https://api.github.com/users/octocat/repos").then(function(response){
+        //json() formats the reponse as JSON
+        response.json().then(function(data) {
+            console.log(data);
+        })
         console.log("inside", response);
     });
    console.log("outside");
