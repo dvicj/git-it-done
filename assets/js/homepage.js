7ca07e3a0c17a836cd87c8196ac113e5b18f55f5
dvicjon <devinvjones@gmail.com>
2021-01-11 16:48:49 -0500

    promise and then

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,4 +1,9 @@
+//this function "fetches" the info (HTTP request) from GitHub API
+//GitHub replies with JSON data
 var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+    fetch("https://api.github.com/users/octocat/repos").then(function(response){
+        console.log("inside", response);
+    });
+   console.log("outside");
 };
 getUserRepos(); 
